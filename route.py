d2f0761c45883e6fd74e154671d43a2e4aef8414
019anshu <anshu.lakra25@gmail.com>
2021-03-14 02:54:06 +0530

    Adding Initial Files

---
+++ route.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask, render_template, flash, redirect, session, url_for
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)   
+
+app.config['SECRET_KEY'] =  os.environ.get('SECRET_KEY')
+
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+    
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500   
+
+if __name__ == '__main__':  
+    app.run(debug=True)
